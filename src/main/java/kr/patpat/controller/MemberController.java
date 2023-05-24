ef9cfcee2aa5eace3e97b284ca4e4ee383ddfca4
gonixxxx <akdntm946@gmail.com>
2023-05-24 16:34:21 +0900

    kakao login2

---
+++ src/main/java/kr/patpat/controller/MemberController.java
@@ -0,0 +1,205 @@
+package kr.patpat.controller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import kr.patpat.entity.Kakao;
+import kr.patpat.mapper.KakaoMapper;
+
+@Controller
+public class MemberController {
+
+	@Autowired
+	private KakaoMapper kakaoMapper;
+
+	// --------------------------------------------------------
+	// 카카오 로그인
+	@RequestMapping("/kakaoLoginForm")
+	public String kakaoLoginForm() {
+		return "member/kakaoLogin";
+	}
+
+	@GetMapping("/kakaoLogin")
+	public String kakaoLogin(@RequestParam("code") String code, Model model, HttpSession session) {
+
+		String accessToken = getAccessToken(code);
+		System.out.println(accessToken);
+
+		String email = getUserInfo(accessToken);
+
+		// JSONPObject kakaoInfo = new JSONPObject(userInfo);
+		// model.addAttribute("kakaoInfo", kakaoInfo);
+
+		session.setAttribute("userEmail", email);
+		session.setAttribute("accessToken", accessToken);
+
+		return "redirect:/";
+
+	}
+
+	@RequestMapping("/kakaoLogout")
+	public String kakaoLogout(HttpSession session) {
+		exeLogout((String) session.getAttribute("accessToken"));
+		session.removeAttribute("accessToken");
+		session.removeAttribute("userEmail");
+
+		return "redirect:/kakaoLoginForm";
+	}
+
+	private String getAccessToken(String code) {
+		String accessToken = "";
+		String refreshToken = "";
+		String reqURL = "https://kauth.kakao.com/oauth/token";
+
+		try {
+			URL url = new URL(reqURL);
+
+			HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+			StringBuilder sb = new StringBuilder();
+			sb.append("grant_type=authorization_code");
+			sb.append("&client_id=08c15618cbd45ecc0b82e9e3777c25b3"); // 본인이 발급받은 key
+			sb.append("&redirect_uri=http://localhost:8081/controller/kakaoLogin"); // 본인이 설정해 놓은 경로
+			sb.append("&code=" + code);
+			bw.write(sb.toString());
+			bw.flush();
+
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			String result = "";
+
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body : " + result);
+
+			JsonParser parser = new JsonParser();
+			JsonElement element = parser.parse(result);
+
+			accessToken = element.getAsJsonObject().get("access_token").getAsString();
+			refreshToken = element.getAsJsonObject().get("refresh_token").getAsString();
+
+			System.out.println("access_token : " + accessToken);
+			System.out.println("refresh_token : " + refreshToken);
+
+			br.close();
+			bw.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return accessToken;
+	}
+
+	private String getUserInfo(String accessToken) {
+		HashMap<String, Object> userInfo = new HashMap<String, Object>();
+
+		String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+
+			// 요청에 필요한 Header에 포함될 내용
+			conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+			String line = "";
+			String result = "";
+
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body : " + result);
+
+			JsonParser parser = new JsonParser();
+			JsonElement element = parser.parse(result);
+
+			JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+			JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+			String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+			String email = kakao_account.getAsJsonObject().get("email").getAsString();
+
+			userInfo.put("nickname", nickname);
+			userInfo.put("email", email);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println(userInfo);
+		System.out.println(userInfo.getClass().getName());
+
+		Kakao kakao = kakaoMapper.selectKakao(userInfo);
+
+		if (kakao != null) {
+			return kakao.getMemEmail();
+		} else {
+			kakaoMapper.kakaoJoin(userInfo);
+			Kakao kakao2 = kakaoMapper.selectKakao(userInfo);
+			return kakao2.getMemEmail();
+		}
+
+	}
+
+	public void exeLogout(String accessToken) {
+		String reqURL = "https://kapi.kakao.com/v1/user/logout";
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+			String result = "";
+			String line = "";
+
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println(result);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/kr/patpat/entity/Kakao.java
@@ -0,0 +1,15 @@
+package kr.patpat.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Kakao {
+	private String memEmail;
+	private String memNickName;
+}
